5b29b6920b69f8c35da7ebefae8e9259c990c8fb
Olga Dubova <ms.olga.dubovaya@gmail.com>
2022-09-04 23:55:35 +0300

    Подключила свайпер для отзывов

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,38 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
+import Swiper, {Navigation, Pagination} from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// const swiper = new Swiper('.swiper', {
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination-custom',
+//     clickable: true
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
